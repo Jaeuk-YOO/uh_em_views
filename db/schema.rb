86a6c24adc29d6063a382fe1622882027b82b262
getitbeauty <getitbeauty@likelion.org>
2017-08-15 22:23:05 +0900

    devise_username

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170813164703) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
