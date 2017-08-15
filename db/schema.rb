a16e22d6a84cb928f7df53f588ccf48012d5329e
susanna16 <jahnne@naver.com>
2017-08-15 22:28:49 +0900

    오류수정

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,13 @@
 
 ActiveRecord::Schema.define(version: 20170815125500) do
 
-
   create_table "review_posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
   create_table "user_locations", force: :cascade do |t|
     t.integer "user_id"
     t.string "location_x"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170815125500) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
